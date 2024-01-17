0d48a89d4f2b45e53bb6430424c507d324d82a62
JoanaPoAlmeida <58987530+JoanaPoAlmeida@users.noreply.github.com>
2024-01-17 15:40:06 +0000

    Final Commit

---
+++ app.js
@@ -0,0 +1,368 @@
+//1 - importamos express
+const express = require('express');
+const app = express();
+
+
+//2 - set urlencoded para guardar os dados do formulário
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use("/public", express.static('public'));
+
+//3 - importar o dotenv
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
+
+//4 - set diretorio publico
+app.use('/resources',express.static('public'));
+app.use('/resources',express.static(__dirname + '/public'));
+
+//5 - estabelecer o ejs
+app.set('view engine','ejs');
+
+//6 - importar modulo para fazer o caching, bcryptjs
+const bcryptjs = require('bcryptjs');
+
+//7 - Variaveis de sessão
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized:true
+}));
+
+//8 - invocamos o modulo de conexao à bd
+const connection = require('./database/db');
+
+//estabelecer rotas
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+app.get('/register', (req, res)=>{
+    res.render('register');
+})
+app.get('/game', (req, res)=>{
+	
+	//Quando atualizar a pagina vai apagar as posiçoes ja inseridas
+	connection.query('DELETE FROM positions',(err,result)=>{
+		if(err) throw err;
+	})
+
+	//vai apagar a pontuacao anterior para começar de novo
+	connection.query('DELETE FROM pontuacao',(err,result)=>{
+		if(err) throw err;
+	})
+
+	//vai criar uma linha na tabela pontuacao com a pontuaçao inicial
+	let sql = "INSERT INTO pontuacao VALUES ('00');"
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;
+	})
+
+	
+	
+	res.render('game');
+})
+
+app.get('/howplay', (req, res)=>{
+    res.render('howplay');
+})
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
+
+
+
+//10 - Registo
+app.post('/register', async (req, res)=>{
+     
+	 const { name, email, pass, passwordConfirm} = req.body;
+     let passwordHaash = await bcryptjs.hash(pass,8);
+
+	 connection.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if(error){
+            console.log(error);
+        }
+        if(results.length > 0) {
+            return res.render('register', {
+                alert: true, 
+                alertTitle: "Email in use",
+                alertMessage: "Oops! Seems like that email is already in use",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer:1500,
+                ruta:''
+            })
+        } else if( pass !== passwordConfirm){
+            return res.render('register', {
+                alert: true, 
+                alertTitle: "Does not match",
+                alertMessage: "Passwords do not match",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer:1500,
+                ruta:''
+            });
+        }
+	
+     connection.query('INSERT INTO users SET ?', {name:name, email:email, pass:passwordHaash}, async(error, results)=>{
+        if(error){
+             console.log(error);
+        }else{
+            res.render('register',{
+                alert: true, 
+                alertTitle: "Registration",
+                alertMessage: "Successful Registration!",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer:1500,
+                ruta:''
+            })
+        }
+     })
+	});
+})
+//11 - Metodo para autenticação
+app.post('/auth', async (req, res)=> {
+	const email = req.body.email;
+	const pass = req.body.pass;    
+    let passwordHaash = await bcryptjs.hash(pass, 8);	
+    if (email && pass) {
+		connection.query('SELECT * FROM users WHERE email = ?', [email], async (error, results, fields)=> {
+			if( results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass)) ) {    
+				res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "Incorrect user or password",
+                        alertIcon:'error',
+                        showConfirmButton: true,
+                        timer: 1500,
+                        ruta: '/login'    
+                    });
+				
+                //res.send('Incorrect Username and/or Password!');				
+			} else {    
+				//criamos uma var de sessao, set true se iniciou sessao        
+				req.session.loggedin = true;                
+				req.session.name = results[0].name;
+				res.render('login', {
+					alert: true,
+					alertTitle: "Conection Successful",
+					alertMessage: "WELCOME TO PIRATE TREASURE",
+					alertIcon:'success',
+					showConfirmButton: false,
+					timer: 1500,
+					ruta: '/'
+				});        			
+			}			
+			res.end();
+		});
+	} else {	
+		res.send('Please enter email and Password!');
+		res.end();
+	}
+});
+//12 - Método para controlar que está auth em todas as páginas
+app.get('/', (req, res)=> {
+	if (req.session.loggedin) {
+		res.render('index',{
+			login: true,
+			name: req.session.name			
+		});		
+	} else {
+		res.render('index',{
+			login:false,
+			name:'Hello!',			
+		});				
+	}
+	res.end();
+});
+
+
+//funçao para limpar a cache dps do logout
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+ //13 - Logout
+//Destroi a sessao
+app.get('/logout', function (req, res) {
+	req.session.destroy(() => {
+	  res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
+	})
+});
+
+
+
+
+//inserir posiçoes das zonas dos inimigos na base de dados
+
+
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.json())
+
+//inserting the positions of the enemy from sketch.js
+app.post('/insertEnemy',(req,res)=>{
+
+	let type="enemy";
+	let x=req.body.posX;
+	let y=req.body.posY;
+	
+	let sql = "INSERT INTO positions (`type`,`posX`,`posY`) Values ('"+type+"','"+x+"','"+y+"');";
+	
+	connection.query(sql,(err,result)=>{
+	if(err) throw err;
+	
+	
+	
+	res.send(result);
+	
+	});
+	
+	});
+
+//getting the positions of the enemy
+app.get('/getEnemy',(req,res)=>{
+
+let type="enemy";
+
+let sql = "SELECT posX,posY FROM positions WHERE type='"+type+"' ORDER BY RAND() limit 6;"
+
+connection.query(sql,(err,result)=>{
+if(err) throw err;
+
+
+
+res.send(result);
+
+});
+
+})
+
+//inserting the positions of the enemy from sketch.js
+app.post('/insertIsland',(req,res)=>{
+
+	let type="island";
+	let x=req.body.posX;
+	let y=req.body.posY;
+	
+	let sql = "INSERT INTO positions (`type`,`posX`,`posY`) Values ('"+type+"','"+x+"','"+y+"');";
+	
+	connection.query(sql,(err,result)=>{
+	if(err) throw err;
+
+	res.send(result);
+	
+	});
+	
+	});
+
+//getting the positions of the enemy
+app.get('/getIsland',(req,res)=>{
+
+let type="island";
+
+let sql = "SELECT posX,posY FROM positions WHERE type='"+type+"' ORDER BY RAND() limit 5;"
+
+connection.query(sql,(err,result)=>{
+if(err) throw err;
+
+
+
+res.send(result);
+
+});
+
+})
+
+//add initial boat position into database
+app.post('/boatPosition', (req,res) =>{
+	let x=req.body.posX;
+	let y=req.body.posY;
+
+	
+	let sql = "INSERT INTO boat VALUES('"+x+"','"+y+"');"
+
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;
+		res.send(result);
+	});
+})
+
+//get boat current position
+app.get('/getBoat', (req,res)=>{
+	let sql="SELECT * FROM boat;";
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;	
+		res.send(result);	
+	});
+})
+
+app.put('/updateBoat', (req,res)=>{
+	let x=req.body.posX;
+	let y=req.body.posY;
+
+
+	let sql="UPDATE boat SET posX='"+x+"', posY='"+y+"';"
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;	
+		res.send(result);	
+	});
+})
+
+//adicionar pontuaçao a base de dados
+app.put('/addPontuacao', (req,res)=>{
+	let pontos=req.body.pont;
+	console.log(pontos);
+	
+
+	let sql="UPDATE pontuacao SET pontos='"+pontos+"' ";
+	
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;	
+	});
+
+	/* let pont = 0;
+	connection.query('SELECT pontos FROM pontuacao', (err,pontos)=>{
+		if(err) throw err;
+		pont=pontos
+
+		console.log("pontos",pont);
+		res.render('game', {pont});
+	})  */ 
+
+})
+
+app.post('/lostButton',(req, res)=>{
+	ponto=req.body.pont;
+	if(ponto==0){
+		console.log("lost:", ponto)
+	}
+})
+
+
+
+app.delete('/deletePositions', (req,res) =>{
+	let type="island";
+	let type2="enemy";
+	let sql = "DELETE FROM positions WHERE TYPE='"+type+"' AND type = '"+type2+"';"
+	connection.query(sql,(err,result)=>{
+		if(err) throw err;	
+		res.send(result);	
+	});
+})
+
+
+
+
+
+
+//console.log(__dirname);
+app.listen(3001,(req, res)=>{
+    console.log('SERVER RUNNING IN http://localhost:3001');
+})
---
+++ public/sketch.js
@@ -0,0 +1,699 @@
+//colocar todos os coments em ingles
+//é necessário dividir isto por varios ficheiros
+//é necessário colocar nuvens em cima das ilhas e inimigos
+//é necessário fazer uma pagina inicial com um botao start, pagina home.js
+//organização mapa da jogo (em papel)
+//é necessário fazer o nível extra
+
+//const { text } = require("body-parser");
+
+//const { text } = require("body-parser");
+
+
+let pontuacao = 0;
+
+//screen siize
+const screenx = 600;
+const screeny = 600;
+
+//array da board
+let arrTiles=[];
+
+//intial postion boat
+let px = 0;
+let py = 0;
+
+//tiles size
+var w=screenx/8;
+var h=screeny/8;
+
+//images
+let island;
+let enemy;
+
+let enemyReceived=[];
+let ilhasReceived=[];
+let enemyExist = false;
+let ilhasExist = false;
+
+let arrEnemies = [];
+let arrIsland = [];
+let images = [];
+
+let song = [];
+
+function preload(){
+
+  //fazer load das imagens
+  images[0]=loadImage('https://i.ibb.co/Ydh3Crs/island.png'); //ilhas
+  images[1]=loadImage('https://i.ibb.co/FmX47db/enemy.png'); //enemy
+  images[2]=loadImage('https://i.ibb.co/K9r38JD/background.png'); //background
+  images[3]=loadImage('https://i.ibb.co/vkLDdmN/boat.png'); //boat
+  images[4]=loadImage('https://i.ibb.co/K5bYFsL/nuvens-1.png') //nuvens
+  images[5]=loadImage('http://localhost:3001/public/img/boatred.jpg')
+
+  //load da musica
+  song[0]= loadSound('http://localhost:3001/public/sound/ptsong.mp3');
+  song[1]= loadSound('http://localhost:3001/public/sound/fail.mp3');
+  song[2]= loadSound('http://localhost:3001/public/sound/win.wav');
+  song[3]= loadSound('http://localhost:3001/public/sound/danger.mp3');
+  song[4]= loadSound('http://localhost:3001/public/sound/hitenemy.mp3');
+  song[5]= loadSound('http://localhost:3001/public/sound/hitilha.wav');
+
+  //adicionar inimigos
+  for(let enmy=0;enmy<6;enmy++){
+      
+        let rx = int(random(0, 7));
+        let ry = int(random(0, 7));
+        let enemy={
+          "posX":rx,
+          "posY":ry,
+        }
+
+        if(enemy.posX == 0 && enemy.posY == 0 || enemy.posX == 1 && enemy.posY == 0 || enemy.posX == 0 && enemy.posY == 1){
+            enmy--;
+            print("proibido",enemy);
+          }else{
+            if (arrEnemies.length > 0) {
+          
+          if (compareEnemy(enemy)) {
+            
+
+            arrEnemies.push(enemy);
+          } else {
+            enmy--;
+            print("igual")
+          }
+        } else {
+          arrEnemies.push(enemy);
+        }
+          }
+        
+        print("adicionado:",enemy)
+
+
+        httpPost('/insertEnemy','json',enemy,(resposta)=>{
+
+        });
+
+        
+      
+    
+  }
+  
+
+  loadJSON('/getEnemy',(dataDoServidor)=>{
+
+    if(dataDoServidor.length>0){
+     
+      enemyReceived=dataDoServidor;
+      console.log("enemies after get:   ", enemyReceived)
+      enemyExist = true;
+      
+    }else{
+      console.log("no enemies");
+    }
+  
+  })
+
+  //adicionar ilhas
+
+  for(let ilh=0;ilh<=5;ilh++){
+
+        let rx = int(random(0, 7));
+        let ry = int(random(0, 7));
+        let island={
+          "posX":rx,
+          "posY":ry,
+        }
+        if (arrIsland.length > 0) {
+          if((island.posX === 0 && island.posY === 0)||(island.posX === 1 && island.posY === 0)||(island.posX === 0 && island.posY === 1)){
+            ilh--;
+            print("proibido ilha",island);
+          }else if (compareIsland(island)) {
+            arrIsland.push(island);
+          } else {
+            ilh--;
+            print("igual")
+          }
+        } else {
+          arrIsland.push(island);
+        }
+        print(island)
+
+      
+        httpPost('/insertIsland','json',island,(resposta)=>{
+
+        });
+  }
+  
+
+
+
+loadJSON('/getIsland',(dataDoServidor)=>{
+
+  if(dataDoServidor.length>0){
+   
+    ilhasReceived=dataDoServidor;
+    console.log("ilhas:   ", ilhasReceived)
+    ilhasExist = true;
+    
+  }else{
+    console.log("no islands");
+  }
+
+})
+}
+
+function compareEnemy(enemy) {
+  let flag = 0;
+
+  for (let i = 0; i < arrEnemies.length; i++) {
+    if (arrEnemies[i].posX == enemy.posX && arrEnemies[i].posY == enemy.posY ) {
+      flag = 1;
+    } 
+  }
+  for (let i = 0; i < ilhasReceived.length; i++) {
+    if (ilhasReceived[i].posX == enemy.posX && ilhasReceived[i].posY == enemy.posY) {
+
+      flag = 1;
+    }
+  }
+
+  if (flag == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function compareIsland(island) {
+  let flag = 0;
+
+  for (let i = 0; i < arrIsland.length; i++) {
+    if (arrIsland[i].posX == island.posX && arrIsland[i].posY == island.posY) {
+      flag = 1;
+    }
+  }
+  //print("from compareisland",arrEnemies)
+  for (let i = 0; i < enemyReceived.length; i++) {
+    if (enemyReceived[i].posX == island.posX && enemyReceived[i].posY == island.posY ) {
+      flag = 1;
+    } 
+  }
+
+  if (flag == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function setup() {
+  createCanvas(screenx, screeny);
+
+  song[0].loop();
+  song[0].setVolume(0.3);
+  
+  
+  
+
+  constructBoard();
+
+  print(arrTiles);
+
+  for(let x=0;x<=7;x++){
+    for(let y=0;y<=7;y++){
+      arrTiles[x][y].img=images[4];
+    }
+  }
+
+
+   
+
+  arrTiles[0][0].img=images[3];
+  let boatpos = {
+    posX: 0,
+    posY: 0
+  }
+  httpDo('/updateBoat','PUT', 'json', boatpos, (response)=>{
+
+  });
+
+  keyPressed();
+}
+
+
+
+
+
+function draw() {
+  //background color
+  background(0);
+  background(145, 187, 199);
+
+
+  //grid 8X8
+  drawBoard();
+
+  textSize(20);
+  text("Pontuação: "+pontuacao, 450, 20);
+  
+}
+
+
+function keyPressed() {
+
+  getAudioContext().resume();
+
+  if (keyCode === RIGHT_ARROW){
+    
+    loadJSON('/getBoat',(dataDoServidor)=>{
+
+      let boat=dataDoServidor;
+      let bx=boat[0].posX;
+      let by=boat[0].posY;
+
+      bx++;
+
+      if(bx<=7){ 
+        let boatpos = {
+          posX:bx,
+          posY:by
+        }
+        if(compareBoatEnemy(boatpos)){
+          song[4].play();
+          
+        }else if(compareBoatIlha(boatpos)){
+          song[5].play();
+        }
+        else if(nearEnemy(boatpos)){
+          print(nearEnemy(boatpos));
+
+          song[3].play();
+          
+          arrTiles[bx][by].img=images[3];
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          bx--;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        else{
+          
+          arrTiles[bx][by].img=images[3]; //adicionar imagem a posiçao a seguir
+
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          bx--;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        //compareBoatIlha
+
+      }else{
+        print("out of sketch");
+      }
+    })
+  }
+
+   if (keyCode === LEFT_ARROW){
+     
+    loadJSON('/getBoat',(dataDoServidor)=>{
+
+      let boat=dataDoServidor;
+
+      let bx=boat[0].posX;
+      let by=boat[0].posY;
+
+      bx--;
+
+      if(bx>=0){
+        
+        let boatpos = {
+          posX:bx,
+          posY:by
+        }
+        if(compareBoatEnemy(boatpos)){
+          song[4].play();
+          
+        }else if(compareBoatIlha(boatpos)){
+          song[5].play();
+        }
+        else if(nearEnemy(boatpos)){
+          print(nearEnemy(boatpos));
+          
+          song[3].play();
+          arrTiles[bx][by].img=images[3];
+
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          bx++;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        else{
+          arrTiles[bx][by].img=images[3]; //adicionar imagem a posiçao a seguir
+
+
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          bx++;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        //compareBoatIlha
+
+      }else{
+        print("out of sketch");
+      }
+    })
+  }
+
+  if (keyCode === DOWN_ARROW){
+    loadJSON('/getBoat',(dataDoServidor)=>{
+
+      let boat=dataDoServidor;
+
+      let bx=boat[0].posX;
+      let by=boat[0].posY;
+
+      by++;
+
+      if(by<=7){
+        
+        let boatpos = {
+          posX:bx,
+          posY:by
+        }
+        if(compareBoatEnemy(boatpos)){
+          song[4].play();
+          
+        }else if(compareBoatIlha(boatpos)){
+          song[5].play();
+        }else if(nearEnemy(boatpos)){
+          
+          song[3].play();
+          arrTiles[bx][by].img=images[3];
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          by--;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        else{
+
+          
+
+          arrTiles[bx][by].img=images[3]; //adicionar imagem a posiçao a seguir
+
+
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          by--;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        
+
+      }else{
+        print("out of sketch");
+      }
+    })
+  }
+
+  if (keyCode === UP_ARROW){
+    loadJSON('/getBoat',(dataDoServidor)=>{
+
+      let boat=dataDoServidor;
+
+      let bx=boat[0].posX;
+      let by=boat[0].posY;
+
+      by--;
+
+      if(by>=0){
+        
+        let boatpos = {
+          posX:bx,
+          posY:by
+        }
+        if(compareBoatEnemy(boatpos)){
+          song[4].play();
+          
+        }else if(compareBoatIlha(boatpos)){
+          song[5].play();
+        }else if(nearEnemy(boatpos)){
+          
+          song[3].play();
+          arrTiles[bx][by].img=images[3];
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          by++;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        else{
+          arrTiles[bx][by].img=images[3]; //adicionar imagem a posiçao a seguir
+
+
+          httpDo('/updateBoat','PUT', 'json', boatpos, (res)=>{
+
+          });
+
+          by++;
+          arrTiles[bx][by].img=images[4]; //posiçao anterior para mar
+        }
+        
+
+      }else{
+        print("out of sketch");
+      }
+    })
+  }
+}
+
+
+
+function compareBoatEnemy(boatpos){
+  let flag = 0;
+  for(let i=0; i<enemyReceived.length; i++){
+    if (enemyReceived[i].posX === boatpos.posX && enemyReceived[i].posY === boatpos.posY) {
+      arrTiles[enemyReceived[i].posX][enemyReceived[i].posY].img=images[1];
+      flag = 1;
+      
+      pontuacao=pontuacao-50;
+      pontuation(pontuacao);
+
+    }
+  }
+  if (flag == 0) {
+    return false; //nao ha enemy naquela posiçao
+  } else {
+    return true; //há enemy naquela posiçao
+  }
+}
+
+function compareBoatIlha(boatpos){
+  let flag = 0;
+
+  
+  for(let i=0; i<ilhasReceived.length; i++){
+    if (ilhasReceived[i].posX === boatpos.posX && ilhasReceived[i].posY === boatpos.posY) {
+      arrTiles[ilhasReceived[i].posX][ilhasReceived[i].posY].img=images[0];
+      flag = 1;
+      
+      pontuacao=pontuacao+100;
+      pontuation(pontuacao);
+
+    }
+  }
+
+  if (flag == 0) {
+    return false; //nao ha enemy naquela posiçao
+  } else {
+    return true; //há enemy naquela posiçao
+  }
+}
+
+function pontuation(pont){
+
+  pontos = {
+    "pont": pont
+  }
+  print(pontos)
+  httpDo('/addPontuacao','PUT', 'json', pontos, (res)=>{
+    print(res);
+  })
+
+  if(pont<=0){
+
+    song[0].stop();
+    song[1].play();
+    
+    for(let x=0;x<=7;x++){
+      for(let y=0;y<=7;y++){
+        arrTiles[x][y].img=images[2];
+        arrTiles[x][y].txt="you lost";
+
+        stroke(0);
+        fill(150 + sin(frameCount * 0.2) * 100);
+        stroke(0);
+        textSize(20);
+        text("YOU LOST", x, y);
+      }
+    }
+
+    if(enemyExist){
+      console.log("enemy", enemyReceived)
+      for(let x=0;x<enemyReceived.length;x++){
+  
+        arrTiles[enemyReceived[x].posX][enemyReceived[x].posY].img=images[1];
+  
+      }
+    }
+    if(ilhasExist){
+      console.log("island", ilhasReceived)
+      for(let x=0;x<ilhasReceived.length;x++){
+  
+        arrTiles[ilhasReceived[x].posX][ilhasReceived[x].posY].img=images[0];
+        
+      }
+    }
+
+    loadJSON('/getBoat',(dataDoServidor)=>{
+
+      let boat=dataDoServidor;
+
+      let bx=boat[0].posX;
+      let by=boat[0].posY;
+
+      arrTiles[bx][by].img=images[3]
+
+    });
+    
+
+    text('YOU LOST', screenx/2, screeny/2);
+
+    httpPost('/lostButton', 'json', pontos, (res)=>{
+      
+    })
+    print("you lost")//show button to restart the game
+  }
+
+  if(pont>=500){
+    song[0].stop();
+    song[2].play();
+    for(let x=0;x<=7;x++){
+      for(let y=0;y<=7;y++){
+        arrTiles[x][y].img=images[2];
+        arrTiles[x][y].txt="you won";
+
+        stroke(0);
+        fill(150 + sin(frameCount * 0.2) * 100);
+        stroke(5);
+        textSize(20);
+        text("YOU WON", x, y);
+      }
+    }
+
+    if(enemyExist){
+      console.log("enemy", enemyReceived)
+      for(let x=0;x<enemyReceived.length;x++){
+  
+        arrTiles[enemyReceived[x].posX][enemyReceived[x].posY].img=images[1];
+  
+      }
+    }
+    if(ilhasExist){
+      console.log("island", ilhasReceived)
+      for(let x=0;x<ilhasReceived.length;x++){
+  
+        arrTiles[ilhasReceived[x].posX][ilhasReceived[x].posY].img=images[0];
+        
+      }
+    }
+  }
+}
+
+
+
+function constructBoard(){
+  
+  for(let x=0;x<=9;x++){
+    arrTiles[x]=[];
+    for(let y=0;y<=9;y++){
+      arrTiles[x][y]= new Tile(x+(x*w),y+(y*h),x,y,w);
+    }
+  }
+  
+}
+
+function drawBoard(){
+  for(let x=0;x<=9;x++){
+    for(let y=0;y<=9;y++){
+      arrTiles[x][y].draw_Tile();
+    }
+  }
+  
+}
+
+class Tile{  
+  constructor(x,y,i,j,s){
+    
+    this.x=x;
+    this.y=y;
+    this.i=i;
+    this.j=j;
+    this.s=s;
+    this.clr="#e0d57b00";
+    this.img=images[2];
+    this.txt="";
+  }
+ 
+  draw_Tile(){
+    text(this.txt,this.x, this.y);
+    image(this.img,this.x,this.y,this.s,this.s);
+    fill(this.clr);
+    square(this.x,this.y,this.s);
+  }
+
+}
+
+
+function nearEnemy(boatpos){
+
+  let flag = 0;
+
+  for(let i=0; i<enemyReceived.length; i++){
+
+    bx = boatpos.posX;
+    by = boatpos.posY;
+
+    if( (bx+1) == enemyReceived[i].posX && by == enemyReceived[i].posY || 
+        bx == enemyReceived[i].posX && by+1 == enemyReceived[i].posY ||
+        (bx-1) == enemyReceived[i].posX && by == enemyReceived[i].posY ||
+        bx == enemyReceived[i].posX && by-1 == enemyReceived[i].posY ){
+      flag = 1;
+      print(boatpos, enemyReceived[i]);
+    }
+  }
+  if (flag == 0) {
+    return false; //nao ha enemy naquela posiçao
+  } else {
+    return true; //há enemy naquela posiçao
+  }
+
+  
+  
+}
+
+
+
